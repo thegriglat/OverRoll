f9efd28f2482abb9e47f70a1b57c08f95ea48355
Grigory Latyshev <thegriglat@gmail.com>
2021-06-10 09:34:39 +0300

    fixup

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: "5px",
+    padding: 5,
   },
   btn: {
     marginLeft: 5,
